7e2c58c55a961e3ed00df04980a1ebb9b7e57c32
joppiesaus <job@function1.nl>
2015-06-01 11:06:56 +0200

    Added password checking

--- do_login.php
+++ do_login.php
@@ -15,11 +15,18 @@ if (!$uid)
 	exit;
 }
 
-session_start();
-$_SESSION["uid"] = $uid;
-
 $user = new User;
 $user->loadById($uid);
+
+if (hash("sha512", $_POST["password"]) != $user->data["profile"]["password"])
+{
+	echo "Wrong password!";
+	exit;
+}
+
+// Why check if someone is logged in already?
+session_start();
+$_SESSION["uid"] = $uid;
 
 echo $user->data["profile"]["name"] . ", you're now logged in!";
 
