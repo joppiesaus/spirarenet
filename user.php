aa480eba719248657e24ff1dcc1a46446f95374a
joppiesaus <job@function1.nl>
2015-06-09 12:05:41 +0200

    Stole a navbar from http://codepen.io/anon/pen/JdNxMY

--- user.php
+++ user.php
@@ -10,31 +10,60 @@
 </head>
 <body>
 
-<div id="nv">
-	<div id="nvwrap">
-		<ul id="nav">
-			<li>Explore
-				<ul>
-					<li><a href="search.php">Search</a></li>
-				</ul>
-			</li>
-			<li>Create
-				<ul>
-					<li><a href="createproperty.php">Property</a></li>
-					<li><a href="createevent.php">Event</a></li>
-					<li><a href="createuser.php">User</a></li>
-				</ul>
-			</li>
-			<li id="nyou">You
-				<ul>
-					<li><a href="#">Profile</a></li>
-					<li><a href="login.php">Login</a></li>
-					<li><a href="logout.php">Logout</a></li>
-				</ul>
-			</li>
-		</ul>
-	</div>
-</div>
+<nav id="primary_nav_wrap">
+<ul>
+  <li class="current-menu-item"><a href="#">Home</a></li>
+  <li><a href="#">Menu 1</a>
+    <ul>
+      <li><a href="#">Sub Menu 1</a></li>
+      <li><a href="#">Sub Menu 2</a></li>
+      <li><a href="#">Sub Menu 3</a></li>
+      <li><a href="#">Sub Menu 4</a>
+        <ul>
+          <li><a href="#">Deep Menu 1</a>
+            <ul>
+              <li><a href="#">Sub Deep 1</a></li>
+              <li><a href="#">Sub Deep 2</a></li>
+              <li><a href="#">Sub Deep 3</a></li>
+                <li><a href="#">Sub Deep 4</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Deep Menu 2</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Sub Menu 5</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Menu 2</a>
+    <ul>
+      <li><a href="#">Sub Menu 1</a></li>
+      <li><a href="#">Sub Menu 2</a></li>
+      <li><a href="#">Sub Menu 3</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Menu 3</a>
+    <ul>
+      <li class="dir"><a href="#">Sub Menu 1</a></li>
+      <li class="dir"><a href="#">Sub Menu 2 THIS IS SO LONG IT MIGHT CAUSE AN ISSEUE BUT MAYBE NOT?</a>
+        <ul>
+          <li><a href="#">Category 1</a></li>
+          <li><a href="#">Category 2</a></li>
+          <li><a href="#">Category 3</a></li>
+          <li><a href="#">Category 4</a></li>
+          <li><a href="#">Category 5</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Sub Menu 3</a></li>
+      <li><a href="#">Sub Menu 4</a></li>
+      <li><a href="#">Sub Menu 5</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Menu 4</a></li>
+  <li><a href="#">Menu 5</a></li>
+  <li><a href="#">Menu 6</a></li>
+  <li><a href="#">Contact Us</a></li>
+</ul>
+</nav>
 
 
 <div id="container">
